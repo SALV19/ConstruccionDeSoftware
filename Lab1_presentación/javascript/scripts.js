4b99c1fec2d132a4fe30f4bdd68e25cf918b8da5
SALV <a01707122@tec.mx>
2025-02-13 13:25:40 -0600

    [ADD] integra javascript prueba

---
+++ Lab1_presentación/javascript/scripts.js
@@ -0,0 +1,10 @@
+console.log("Hello there");
+console.info("Hey! you should reply with General Kenobi");
+console.warn("If you don't, somehting might happen");
+console.error("This is what happens when you don't say General Kenobi");
+
+var inseguro = "vas mal";
+let mejor = "variable";
+const precio = 99.99;
+
+const ecuacion_booleana = {} == [] || [] == 0;
