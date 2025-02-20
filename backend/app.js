8eceb154abcb59786fdd9c2d75be127db2ff08ef
SALV <a01707122@tec.mx>
2025-02-20 12:51:59 -0600

    [ADD] ejemplo timeout

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,11 @@
 const file_system = require("fs");
 
-file_system.writeFileSync("hola.txt", "Hola mi buen amigo");
+file_system.writeFileSync("hola.html", "Hola mi buen amigo");
+
+const arreglo = [199, 121, 434, 12, 54, 12, 54, 65, 7];
+
+arreglo.forEach((a) => {
+  setTimeout(() => {
+    console.log(a);
+  }, a);
+});
