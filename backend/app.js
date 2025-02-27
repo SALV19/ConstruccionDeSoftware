f53257fdab31735ddd231257bd17b95f1807b9a3
SALV <a01707122@tec.mx>
2025-02-27 12:08:18 -0600

    [ADD] path

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,9 @@ app.use("/plantas", plantas_routes);
 app.use("/", other_routes);
 
 app.use((req, res, next) => {
+  res.status = 404;
   res.send("ERROR: 404");
 });
 
+console.log("Server started in: http://localhost:3000");
 app.listen(3000);
