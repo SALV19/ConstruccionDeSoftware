de99177ede7a0108e9dea3467c32822cb8fe5bb5
SALV <a01707122@tec.mx>
2025-03-03 12:26:14 -0600

    [FIX] plantas ejs / controller render submitted plantss

--- backend/controllers/plantas.controller.js
+++ backend/controllers/plantas.controller.js
@@ -1,26 +1,15 @@
+const fs = require("fs");
+
 exports.get_agregar = (req, res, next) => {
-  response.render("agregar_planta");
+  res.render("agregar_plantas");
 }
+
+const plantas = []
 
 exports.post_agregar = (req, res, next) => {
   console.log(req.body.planta);
   plantas.push(req.body.planta);
   fs.appendFileSync("plantas.txt", req.body.planta + "\n");
   const plantas_r = fs.readFileSync("plantas.txt")
-  console.log(plantas_r);
-  let html = html_header;
-  html += "<div class='section'>";
-  for (let p of plantas) {
-    html += `
-    <div class="card">  <div class="card-content">
-          <div class="media-content">
-            <p class="title is-4">${p}</p>
-          </div>
-        </div>
-        </div>`;
-  }
-  html += "</div>";
-  html += html_footer;
-  res.send(html);
-  console.log(plantas);
+  res.render("plants", {plantas: plantas})
 }
