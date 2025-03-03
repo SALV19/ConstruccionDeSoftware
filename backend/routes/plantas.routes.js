580cf84d616be40515d175afd339a381167958dd
SALV <a01707122@tec.mx>
2025-03-03 12:27:50 -0600

    [ADD] two routes same logic

--- backend/routes/plantas.routes.js
+++ backend/routes/plantas.routes.js
@@ -3,6 +3,7 @@ const plantas_controller = require("../controllers/plantas.controller");
 const router = express.Router();
 
 router.get("/agregar", plantas_controller.get_agregar);
+router.get("/add", plantas_controller.get_agregar);
 router.post("/agregar", plantas_controller.post_agregar);
 
 module.exports = router;
