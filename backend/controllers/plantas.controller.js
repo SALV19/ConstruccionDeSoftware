7713acf06e59e43082ac3321aa5ec2d7cfa5a819
SALV <a01707122@tec.mx>
2025-03-04 13:15:52 -0600

    [ADD] cookies / fin lab14

--- backend/controllers/plantas.controller.js
+++ backend/controllers/plantas.controller.js
@@ -7,15 +7,20 @@ exports.get_root = (req, res, next) => {
     isLoggedIn: req.session.isLoggedIn || false,
     user: req.session.username,
   });
+  console.log(req.get("Cookie"));
 };
 
 exports.get_agregar = (req, res, next) => {
-  res.render("agregar_plantas");
+  res.render("agregar_plantas", {
+    isLoggedIn: req.session.isLoggedIn || false
+  });
 };
 
 exports.post_agregar = (req, res, next) => {
-  const mi_planta = new Planta(req.body.username);
+  console.log(req.body.planta)
+  const mi_planta = new Planta(req.body.planta);
   mi_planta.save();
 
+  res.setHeader('Set-Cookie', `ultima-planta=${req.body.planta}`)
   res.redirect("/plantas/");
 };
